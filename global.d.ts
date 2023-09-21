c97ab93eda033cf02e6754e00dfec77bafa60655
Moishi Netzer <moishinetzer@gmail.com>
2023-09-21 02:38:13 +0100

    with modules css

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,6 @@
-declare module "*.css";
+declare module "*.module.css" {
+  const styles: {
+    readonly [key: string]: string;
+  };
+  export default styles;
+}
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,8 +1,9 @@
 import dts from "bun-plugin-dts";
+import lightningcss from "bun-lightningcss";
 
 await Bun.build({
   entrypoints: ["lib/index.ts"],
   outdir: "dist",
-  plugins: [dts()],
+  plugins: [dts(), lightningcss()],
   external: ["react", "react-dom"],
 });
